8c329551074e151a983df8c08d9b1a0e091d1a87
Vinodh kumar <kumarvinodh456@gmail.com>
2025-08-15 10:40:11 +0530

    Update View.jsx

--- frontend/src/component/salary/View.jsx
+++ frontend/src/component/salary/View.jsx
@@ -12,7 +12,7 @@ const View = () => {
 
     const fetchSalaries = async () => {
         try {
-            const response = await axios.get(`http://localhost:5000/api/salary/${id}/${user.role}`, {
+            const response = await axios.get(`https://attendance-management-system-backend-qzzf.onrender.com/api/salary/${id}/${user.role}`, {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem("token")}`,
                 },
