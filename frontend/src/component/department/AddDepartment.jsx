aed6c7e69e0bbfbc31b0ced83798b2bef15d5204
Vinodh kumar <kumarvinodh456@gmail.com>
2025-08-15 11:12:29 +0530

    Update AddDepartment.jsx

--- frontend/src/component/department/AddDepartment.jsx
+++ frontend/src/component/department/AddDepartment.jsx
@@ -16,7 +16,7 @@ const AddDepartment = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const response = await axios.post('http://localhost:5000/api/department/add', department, {
+            const response = await axios.post(`https://attendance-management-system-backend-qzzf.onrender.com/api/department/add', department, {
                 headers: {
                     "Authorization": `Bearer ${localStorage.getItem('token')}`
                 }
