9d4838008540d8a43816514feaecb8fa0902bc7a
Vinodh kumar <kumarvinodh456@gmail.com>
2025-08-15 11:11:11 +0530

    Update DepartmentList.jsx

--- frontend/src/component/department/DepartmentList.jsx
+++ frontend/src/component/department/DepartmentList.jsx
@@ -17,7 +17,7 @@ fetchDepartments()
     const fetchDepartments = async () => {
       setDepLoading(true)
       try {
-        const response = await axios.get('http://localhost:5000/api/department', {
+        const response = await axios.get('https://attendance-management-system-backend-qzzf.onrender.com/api/department', {
           headers: {
             "Authorization": `Bearer ${localStorage.getItem('token')}`
           }
