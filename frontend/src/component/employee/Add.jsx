7f69a631a0252def680ec632243c40ef126f8475
Vinodh kumar <kumarvinodh456@gmail.com>
2025-08-15 10:35:45 +0530

    Update Add.jsx

--- frontend/src/component/employee/Add.jsx
+++ frontend/src/component/employee/Add.jsx
@@ -32,7 +32,7 @@ const handleSubmit = async (e) => {
   });
 
   try {
-    const response = await axios.post('http://localhost:5000/api/employee/add', formDataObj, {
+    const response = await axios.post('https://attendance-management-system-backend-qzzf.onrender.com/api/employee/add', formDataObj, {
       headers: {
         "Authorization": `Bearer ${localStorage.getItem('token')}`,
         "Content-Type": "multipart/form-data"  // Add this line
