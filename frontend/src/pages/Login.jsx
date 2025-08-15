cd139e180f4a300a1389e065dd34a312189ba9ab
Vinodh kumar <kumarvinodh456@gmail.com>
2025-08-15 10:28:38 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     e.preventDefault()
     // alert("ok")
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/login", { email, password })
+      const response = await axios.post("https://attendance-management-system-backend-qzzf.onrender.com/api/auth/login", { email, password })
       if (response.data.success) {
         login(response.data.user)
         localStorage.setItem("token", response.data.token)
