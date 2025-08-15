3d595b8e2cd62bc066e7387b7006cd40c29b6b68
Vinodh kumar <kumarvinodh456@gmail.com>
2025-08-15 10:20:43 +0530

    Update DepartmentHelper.jsx

--- frontend/src/utils/DepartmentHelper.jsx
+++ frontend/src/utils/DepartmentHelper.jsx
@@ -26,7 +26,89 @@ export const DepartmentButtons = ({Id,onDepartmentDelete}) =>{
         const confirm = window.confirm("Do you Want to delete")
         if(confirm){
 try {
-  const response = await axios.delete(`http://localhost:5000/api/department/${id}`,{
+  const response = await axios.delete(`import axios from 'axios';
+import React from 'react'
+
+
+export const columns = [
+  {
+    name: "S No",
+    selector: (row) => row.sno,
+        sortable: true,
+    width: "130px"
+  },
+  {
+    name: "Name",
+    selector: (row) => row.name,
+    sortable: true,
+    width : "270px"
+  },
+  {
+    name: "Emp Id",
+    selector: (row) => row.employeeId,
+    sortable: true,
+    width : "150px"
+  },
+
+  {
+    name: "Department",
+    selector: (row) => row.department,
+    width: "190px"
+  },
+
+  {
+    name: "Action",
+    selector: (row) => row.action,
+    center: "true"
+  },
+];
+
+
+ export const AttendanceHelper = ({ status, employeeId,statusChange}) => {
+    const markEmployee = async (status, employeeId) =>{
+  const response = await axios.put(`https://attendance-management-system-backend-qzzf.onrender.com/api/attendance/update/${employeeId}`, {status}, {
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+    },
+
+  })
+
+  if(response.data.success){
+    statusChange();
+  }
+
+    }
+  return (
+    <div>
+     {status ==null ? (
+     <div className='flex space-x-8'>
+        <button className='px-4 py-2 bg-green-500 text-white'
+        onClick={() => markEmployee("present",employeeId)}
+        >Present
+        </button>
+
+        <button className='px-4 py-2 bg-red-500 text-white'
+        onClick={() => markEmployee("Absent",employeeId)}
+        >Absent
+        </button>
+
+        <button className='px-4 py-2 bg-amber-500 text-white'
+        onClick={() => markEmployee("Sick",employeeId)}
+        >Sick
+        </button>
+
+
+        <button className='px-4 py-2 bg-blue-500 text-white'
+        onClick={() => markEmployee("Leave",employeeId)}
+        >Leave
+        </button>
+        </div>
+      )  : (<p className='bg-gray-100 w-20 text-center py-2 rounded'>{status}</p>
+        )}
+    </div>
+  )
+}
+/api/department/${id}`,{
     headers:{
       "Authorization": `Bearer ${localStorage.getItem('token')}`
     }
